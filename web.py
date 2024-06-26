2aa1c0e2f8abb675a57bb1c04d38aaf5940a7355
AndreyShatukho <117357610+AndreyShatukho@users.noreply.github.com>
2024-06-26 14:55:48 +0300

    web.py

--- web.py
+++ web.py
@@ -1,5 +1,4 @@
 from flask import Flask, render_template, request
-import ast
 import Levenshtein
 
 app = Flask(__name__)
@@ -8,7 +7,7 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
-@app.route('/check_plagiarism', methods=['POST'])
+@app.route('/check', methods=['POST'])
 def check_plagiarism():
     file1 = request.files['file1']
     file2 = request.files['file2']
@@ -19,16 +18,7 @@ def check_plagiarism():
     distance = Levenshtein.distance(code1, code2)
     similarity = (1 - (distance / max(len(code1), len(code2)))) * 100
 
-    try:
-        tree1 = ast.parse(code1)
-        tree2 = ast.parse(code2)
-    except SyntaxError as e:
-        return render_template('result.html', error_message='Ошибки синтаксиса в одном или обоих файлах.')
-
-    ast_str1 = ast.dump(tree1)
-    ast_str2 = ast.dump(tree2)
-
-    return render_template('result.html', similarity=similarity, ast_str1=ast_str1, ast_str2=ast_str2)
+    return render_template('result.html', similarity=similarity)
 
 if __name__ == '__main__':
     app.run()
