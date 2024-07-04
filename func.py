710be7fb6770db33cd08beb880af35a4aca9c465
AndreyShatukho <117357610+AndreyShatukho@users.noreply.github.com>
2024-07-04 13:51:42 +0300

    Add files via upload

--- func.py
+++ func.py
@@ -1,30 +1,48 @@
-from pygments import highlight
-from pygments.lexers import PythonLexer
-from pygments.formatters import HtmlFormatter
-import difflib
+import Levenshtein
+import re
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.metrics.pairwise import cosine_similarity
 
-def convert_to_html(input_file):
-    output_file = input_file[:-3] + ".html"
+def remove_comments(code):
+    code = re.sub(r'\/\/.*', '', code)
+    code = re.sub(r'\/\*(.|\n)*?\*\/', '', code)
+    return code
 
-    with open(input_file, "r") as file:
-        code = file.read()
+def normalize(code):
+    code = remove_comments(code)
+    lines = code.split('\n')
+    filtered = [line for line in lines if not line.startswith('import') and not line.startswith('from')]
+    normalized_code = '\n'.join(filtered)
+    #normalized_code = re.sub(r'\b[a-zA-Z_]\w*\b', 'VAR_NAME', normalized_code)
+    return normalized_code, filtered
 
-    lexer = PythonLexer()
-    formatter = HtmlFormatter(style="colorful")
+def find_similar_lines(code1, code2, original_code1, original_code2):
+    lines1 = code1.split('\n')
+    lines2 = code2.split('\n')
 
-    highlighted_code = highlight(code, lexer, formatter)
+    similar_lines = []
 
-    with open(output_file, "w") as file:
-        file.write(highlighted_code)
+    for line_num, (line1, original_line1) in enumerate(zip(lines1, original_code1), start=1):
+        for line_num2, (line2, original_line2) in enumerate(zip(lines2, original_code2), start=1):
+            stripped_line1 = line1.strip()
+            stripped_line2 = line2.strip()
 
-def compare_files(file1, file2):
-    with open(file1, "r") as file:
-        html1 = file.readlines()
+            if stripped_line1 and stripped_line2:
+                normalized_line1 = re.sub(r'\b\w+\b', 'var_name', original_line1)
+                normalized_line2 = re.sub(r'\b\w+\b', 'var_name', original_line2)
+                if normalized_line1 == normalized_line2:
+                    similar_lines.append((original_line1, original_line2, line_num, line_num2))
 
-    with open(file2, "r") as file:
-        html2 = file.readlines()
+    return similar_lines
 
-    diff = difflib.SequenceMatcher(None, html1, html2)
-    similarity = diff.ratio()
+def calculate_similarity_leven(code1, code2):
+    distance = Levenshtein.distance(code1, code2)
+    similarity = (1 - (distance / max(len(code1), len(code2)))) * 100
+    return similarity
 
-    return similarity * 100
+def calculate_similarity_cosine(code1, code2):
+    vectorizer = TfidfVectorizer()
+    tfidf_matrix = vectorizer.fit_transform([code1, code2])
+    similarity_matrix = cosine_similarity(tfidf_matrix[0], tfidf_matrix[1])
+    similarity = similarity_matrix[0][0] * 100
+    return similarity
--- web.py
+++ web.py
@@ -1,49 +1,9 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for, session, send_from_directory
 import Levenshtein
 import re
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
-
-def remove_comments(code):
-    code = re.sub(r'\/\/.*', '', code)
-    code = re.sub(r'\/\*(.|\n)*?\*\/', '', code)
-    return code
-
-def normalize(code):
-    code = remove_comments(code)
-    lines = code.split('\n')
-    filtered = [line for line in lines if not line.startswith('import') and not line.startswith('from')]
-    normalized_code = '\n'.join(filtered)
-    #normalized_code = re.sub(r'\b[a-zA-Z_]\w*\b', 'VAR_NAME', normalized_code)
-    return normalized_code, filtered
-
-def find_similar_lines(code1, code2, original_code1, original_code2):
-    lines1 = code1.split('\n')
-    lines2 = code2.split('\n')
-
-    similar_lines = []
-
-    for line1, original_line1 in zip(lines1, original_code1):
-        for line2, original_line2 in zip(lines2, original_code2):
-            stripped_line1 = line1.strip()
-            stripped_line2 = line2.strip()
-
-            if stripped_line1 and stripped_line2 and stripped_line1 == stripped_line2:
-                similar_lines.append((original_line1, original_line2))
-
-    return similar_lines
-
-def calculate_similarity_leven(code1, code2):
-    distance = Levenshtein.distance(code1, code2)
-    similarity = (1 - (distance / max(len(code1), len(code2)))) * 100
-    return similarity
-
-def calculate_similarity_cosine(code1, code2):
-    vectorizer = TfidfVectorizer()
-    tfidf_matrix = vectorizer.fit_transform([code1, code2])
-    similarity_matrix = cosine_similarity(tfidf_matrix[0], tfidf_matrix[1])
-    similarity = similarity_matrix[0][0] * 100
-    return similarity
+from func import remove_comments, normalize,find_similar_lines, calculate_similarity_leven, calculate_similarity_cosine
 
 app = Flask(__name__)
 
@@ -63,9 +23,17 @@ def check_plagiarism():
 
     similarity_leven = calculate_similarity_leven(normalized1, normalized2)
     similarity_cosine = calculate_similarity_cosine(normalized1, normalized2)
+
     similar_lines = find_similar_lines(normalized1, normalized2, normalized11, normalized21)
 
-    return render_template('result.html', similarity_leven=similarity_leven, similarity_cosine=similarity_cosine, similar_lines=similar_lines)
+    show_image = similarity_cosine > 70
+
+    return render_template('result.html', similarity_leven=similarity_leven, similarity_cosine=similarity_cosine,
+                           similar_lines=similar_lines, show_image=show_image)
+
+@app.route("/image.jpg")
+def image():
+    return render_template("image.jpg")
 
 if __name__ == '__main__':
-    app.run()
+    app.run(port=65)
